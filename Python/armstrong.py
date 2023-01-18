fc10fa86689f4cbcd329bd0a2a68e5117167ff6d
Bakarseck <seck.bakar@ugb.edu.sn>
2023-01-18 16:16:22 +0000

    Using python Language

--- Python/armstrong.py
+++ Python/armstrong.py
@@ -9,16 +9,4 @@ def Armstrong(nbr):
         print(nbr,"est un nombre Armstrong")
     else:
         print(nbr,"n'est pas un nombre Armstrong")
-
-    # listOfIntegersInN = []
-    # sommeCubique = 0
-    # while n != 0:
-    #     listOfIntegersInN.append(n%10)
-    #     n = n // 10
-    # for i in listOfIntegersInN:
-    #     sommeCubique = sommeCubique + i**3
-    # if n == sommeCubique:
-    #     return True
-    # elif n != sommeCubique:
-    #     return False
 Armstrong(153)
